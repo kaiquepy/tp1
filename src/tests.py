b3d177b083d000370f299ca2c4824caefb7000cb
Kaique Vieira <kvieiramiranda@gmail.com>
2024-05-14 19:06:26 -0300

    adição da função de teste da árvore avl

--- src/tests.py
+++ src/tests.py
@@ -5,6 +5,8 @@ Arquivo que contém os testes do estruturas de dados
 import random
 import time
 from modules.arvore_binaria import ArvoreBinaria, retorna_tipo_reg
+from modules.arvore_avl import ArvoreAVL
+
 
 def arvore_binaria_test() -> None:
     """_summary_
@@ -15,8 +17,8 @@ def arvore_binaria_test() -> None:
     nome_arquivo = "arquivos_entrada/100.txt"
     total_presente = 1
     total_ausente = 1
-    vetor_encontradas = ["" for _ in range(16)]
-    vetor_nao_encontradas = ["" for _ in range(16)]
+    vetor_encontradas = []
+    vetor_nao_encontradas = []
 
     arvore.raiz = None
 
@@ -41,14 +43,12 @@ def arvore_binaria_test() -> None:
         end_time = time.time()
         elapsed_time = end_time - start_time
 
-        if resultado_chave_encontrada:
-            if total_presente <= 15:
-                vetor_encontradas[total_presente - 1] = f"Chave ({chave_aleatoria:06}) encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arvore.numero_interacoes}"
-                total_presente += 1
-        else:
-            if total_ausente <= 15:
-                vetor_nao_encontradas[total_ausente - 1] = f"Chave ({chave_aleatoria:06}) não encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arvore.numero_interacoes}"
-                total_ausente += 1
+        if resultado_chave_encontrada and total_presente <= 15:
+            vetor_encontradas.append(f"Chave ({chave_aleatoria:06}) encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arvore.numero_interacoes}")
+            total_presente += 1
+        elif total_ausente <= 15:
+            vetor_nao_encontradas.append(f"Chave ({chave_aleatoria:06}) não encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arvore.numero_interacoes}")
+            total_ausente += 1
 
         gerou_todas = total_ausente == 16 and total_presente == 16
 
@@ -59,48 +59,46 @@ def arvore_binaria_test() -> None:
             arquivo_saida.write(vetor_nao_encontradas[i] + "\n")
 
 
-arvore_binaria_test()
+def arvore_avl() -> None:
+    """_summary_
+    """
+    random.seed(time.time())
+    arvore = ArvoreAVL()
+    nome_arquivo = "arquivos_entrada/100.txt"
 
-#def arvore_avl():
-#    random.seed(time.time())
-#    arv = ArvoreAVL()
-#    nomeArquivo = "dadosOrdenados500.txt"
-#
-#    with open(os.path.join("Arquivos Entrada", nomeArquivo), 'r') as arquivo:
-#        for linha in arquivo:
-#            linha = linha.strip()
-#            if linha:
-#                registro_temp = retornaTipoReg(linha)
-#                arv.raiz = arv.inserir(arv.raiz, registro_temp)
-#
-#    vetorEncontradas = []
-#    vetorNaoEncontradas = []
-#    totalPresente = 0
-#    totalAusente = 0
-#    gerouTodas = False
-#
-#    while not gerouTodas:
-#        chaveAleatoria = random.randint(0, 19999) if totalAusente < 15 else random.randint(0, 9999)
-#        arv.numero_interacoes = 0
-#
-#        start_time = time.time()
-#        resultadoChaveEncontrada = arv.buscar(arv.raiz, chaveAleatoria)
-#        elapsed_time = time.time() - start_time
-#
-#        if resultadoChaveEncontrada:
-#            if totalPresente < 15:
-#                vetorEncontradas.append(f"Chave ({chaveAleatoria:06}) encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arv.numero_interacoes}")
-#                totalPresente += 1
-#        else:
-#            if totalAusente < 15:
-#                vetorNaoEncontradas.append(f"Chave ({chaveAleatoria:06}) não encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arv.numero_interacoes}")
-#                totalAusente += 1
-#
-#        gerouTodas = (totalAusente == 15) and (totalPresente == 15)
-#
-#    with open(os.path.join("Arquivos Saida", "avl", f"arquivo_saida_{nomeArquivo}"), 'w') as arquivo_saida:
-#        for linha in vetorEncontradas:
-#            arquivo_saida.write(linha + "\n")
-#        for linha in vetorNaoEncontradas:
-#            arquivo_saida.write(linha + "\n")
-#
+    with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            registro_temp = retorna_tipo_reg(linha)
+            arvore.raiz = arvore.inserir(arvore.raiz, registro_temp)
+
+    vetor_encontradas = []
+    vetor_nao_encontradas = []
+    total_presente = 0
+    total_ausente = 0
+    gerou_todas = False
+
+    while not gerou_todas:
+        chave_aleatoria = random.randint(0, 19999) if total_ausente < 15 else random.randint(0, 9999)
+        arvore.numero_interacoes = 0
+
+        start_time = time.time()
+        resultado_chave_encontrada = arvore.buscar(arvore.raiz, chave_aleatoria)
+        elapsed_time = time.time() - start_time
+
+        if resultado_chave_encontrada:
+            if total_presente < 15:
+                vetor_encontradas.append(f"Chave ({chave_aleatoria:06}) encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arvore.numero_interacoes}")
+                total_presente += 1
+        else:
+            if total_ausente < 15:
+                vetor_nao_encontradas.append(f"Chave ({chave_aleatoria:06}) não encontrada na árvore. Tempo de busca: {elapsed_time:.9f} segundos. Interações: {arvore.numero_interacoes}")
+                total_ausente += 1
+
+        gerou_todas = (total_ausente == 15) and (total_presente == 15)
+
+    with open("arquivos_saida/100.txt", "w", encoding="utf-8") as arquivo_saida:
+        for linha in vetor_encontradas:
+            arquivo_saida.write(linha + "\n")
+        for linha in vetor_nao_encontradas:
+            arquivo_saida.write(linha + "\n")
