9b9d08633c58844dbc34ad9aaabfedbcef69abd0
Kaique Vieira <kvieiramiranda@gmail.com>
2024-05-14 14:58:31 -0300

    implementando geração do dado2

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,7 +2,7 @@
 Arquivo com funções uteis que podem ser reutilizadas
 """
 
-import time, random
+import time, random, string
 
 
 def tempo_gasto(funcao):
@@ -16,13 +16,14 @@ def tempo_gasto(funcao):
 
 def gerar_arquivo(linhas: int, chaves_ordenadas: bool) -> None:
   nome_do_arquivo = f'arquivos_entrada/{linhas}_desordenado.txt'
-  ids = random.sample(range(linhas), linhas)
+  chave = random.sample(range(linhas), linhas)
 
   if chaves_ordenadas:
     nome_do_arquivo = f'arquivos_entrada/{linhas}.txt'
-    ids = range(linhas)
+    chave = range(linhas)
 
   with open(nome_do_arquivo, "w") as arquivo:
     for i in range(linhas):
-      linha = f'{ids[i]},12345,AOEUAOEUASTOEUHAOSENTUHAOSENTUHAS\n'
+      dado2 = ''.join(random.choices(string.ascii_uppercase, k=1000))
+      linha = f'{chave[i]},12345,{dado2}\n'
       arquivo.write(linha)
